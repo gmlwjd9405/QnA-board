96b0ae27c803d4f6a3d9ed1f6aebeb4e3a11c1d1
kwonheejeong <gmlwjd9405@naver.com>
2018-11-21 16:20:34 +0900

    Implement login

--- src/main/java/com/hee/domain/User.java
+++ src/main/java/com/hee/domain/User.java
@@ -25,6 +25,10 @@ public class User {
         this.password = password;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
