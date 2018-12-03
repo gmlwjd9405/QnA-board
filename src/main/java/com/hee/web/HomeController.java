480d148e5952c8d042a709ca239174cd42162c76
kwonheejeong <gmlwjd9405@naver.com>
2018-12-03 21:01:33 +0900

    Implement show questions list

--- src/main/java/com/hee/web/HomeController.java
+++ src/main/java/com/hee/web/HomeController.java
@@ -1,14 +1,23 @@
 package com.hee.web;
 
+import com.hee.domain.QuestionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 //@RestController
 @Controller
 //@ResponseBody
 public class HomeController {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
     @GetMapping("")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("questions", questionRepository.findAll());
+
         return "index";
     }
 }
