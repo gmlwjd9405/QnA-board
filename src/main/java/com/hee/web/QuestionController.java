8b71b67f50919b932270deaa4f74556cf2a12495
kwonheejeong <gmlwjd9405@naver.com>
2018-12-04 15:13:22 +0900

    Implement detail question list

--- src/main/java/com/hee/web/QuestionController.java
+++ src/main/java/com/hee/web/QuestionController.java
@@ -5,7 +5,9 @@ import com.hee.domain.QuestionRepository;
 import com.hee.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -36,4 +38,10 @@ public class QuestionController {
         return "redirect:/";
     }
 
+    @GetMapping("/{id}")
+    public String show(@PathVariable Long id, Model model) {
+        model.addAttribute("question", questionRepository.findById(id).orElse(null));
+
+        return "/qna/show";
+    }
 }
