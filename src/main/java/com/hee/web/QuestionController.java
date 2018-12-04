b19ab527faa59138d14d24b5b52bded77b7e34a4
kwonheejeong <gmlwjd9405@naver.com>
2018-12-04 17:57:24 +0900

    Implement question update

--- src/main/java/com/hee/web/QuestionController.java
+++ src/main/java/com/hee/web/QuestionController.java
@@ -6,10 +6,7 @@ import com.hee.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
@@ -43,5 +40,21 @@ public class QuestionController {
         model.addAttribute("question", questionRepository.findById(id).orElse(null));
 
         return "/qna/show";
+    }
+
+    @GetMapping("/{id}/form")
+    public String updateForm(@PathVariable Long id, Model model) {
+        model.addAttribute("question", questionRepository.findById(id).orElse(null));
+
+        return "/qna/updateForm";
+    }
+
+    @PutMapping("/{id}")
+    public String update(@PathVariable Long id, String title, String contents) {
+        Question question = questionRepository.findById(id).orElse(null);
+        question.update(title, contents);
+        questionRepository.save(question);
+
+        return String.format("redirect:/questions/%d",id);
     }
 }
